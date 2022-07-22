07d6bf67f21d7845e78b6c88c2d806cef660c0c1
ralvescsota <rafael.rac.mg@gmail.com>
2022-07-22 06:12:22 -0300

    feat: async

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,58 @@
-use rumqttc::{self, Client, LastWill, MqttOptions, QoS};
-use std::thread;
+use tokio::{task, time};
+
+use rumqttc::{self, AsyncClient, Event, MqttOptions, Packet, QoS};
+use std::error::Error;
 use std::time::Duration;
 
-fn main() {
+#[tokio::main(worker_threads = 1)]
+async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
+    // color_backtrace::install();
 
     let mut mqtt_options = MqttOptions::new("test-1", "localhost", 1883);
     mqtt_options
         .set_credentials("mqtt_user", "password")
         .set_keep_alive(Duration::from_secs(5));
 
-    let (client, mut connection) = Client::new(mqtt_options, 10);
+    let (client, mut eventloop) = AsyncClient::new(mqtt_options, 50);
+    task::spawn(async move {
+        requests(client).await;
+        time::sleep(Duration::from_secs(10)).await;
+    });
 
-    //
-    thread::spawn(move || publish(client));
-
-    //
-    for (i, notification) in connection.iter().enumerate() {
-        println!("{}. Notification = {:?}", i, notification);
+    loop {
+        if let Ok(e) = eventloop.poll().await {
+            println!("Event::");
+            if let Event::Incoming(inc) = e {
+                println!("Incoming::");
+                if let Packet::Publish(res) = inc {
+                    println!("Publish::");
+                    println!("{:?}", res)
+                } else {
+                    println!("{:?}\n", inc.clone());
+                }
+            } else {
+                println!("Outgoing::");
+                println!("{:?}\n", e.clone());
+            }
+        }
     }
-
-    println!("Done with the stream!!");
 }
 
-fn publish(mut client: Client) {
-    client.subscribe("hello/+/world", QoS::AtMostOnce).unwrap();
-    for i in 0..10_usize {
-        let payload = vec![1; i];
-        let topic = format!("hello/{}/world", i);
-        let qos = QoS::AtLeastOnce;
+async fn requests(client: AsyncClient) {
+    client
+        .subscribe("hello/world", QoS::AtMostOnce)
+        .await
+        .unwrap();
 
-        client.publish(topic, qos, false, payload).unwrap();
+    for i in 1..=10 {
+        client
+            .publish("hello/world", QoS::AtMostOnce, false, vec![1; i])
+            .await
+            .unwrap();
+
+        time::sleep(Duration::from_secs(1)).await;
     }
 
-    thread::sleep(Duration::from_secs(1));
+    time::sleep(Duration::from_secs(120)).await;
 }
