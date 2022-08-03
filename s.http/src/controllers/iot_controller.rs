5c2fe2b968ec1399393e15ddf4daa8168dbe3276
ralvescsota <rafael.rac.mg@gmail.com>
2022-08-03 05:37:56 -0300

    feat: simple http server

--- s.http/src/controllers/iot_controller.rs
+++ s.http/src/controllers/iot_controller.rs
@@ -1,14 +1,7 @@
-use actix_web::{get, web, Error, HttpResponse};
-use json::JsonValue;
+use crate::{errors::iot::HttpError, viewmodels::iot_viewmodel::MyObj};
+use actix_web::{get, web, HttpResponse, Result};
 
 #[get("/iot")]
-pub async fn get(body: web::Bytes) -> Result<HttpResponse, Error> {
-    let result = json::parse(std::str::from_utf8(&body).unwrap());
-    let injson: JsonValue = match result {
-        Ok(v) => v,
-        Err(e) => json::object! {"err" => e.to_string() },
-    };
-    Ok(HttpResponse::Ok()
-        .content_type("application/json")
-        .body(injson.dump()))
+pub async fn get(body: web::Json<MyObj>) -> Result<HttpResponse, HttpError> {
+    Ok(HttpResponse::Ok().json(body.0))
 }
