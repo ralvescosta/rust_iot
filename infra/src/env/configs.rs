f1856bf9bc963726aa4c3a1936529f8b794d57b0
ralvescsota <rafael.rac.mg@gmail.com>
2022-08-05 06:41:33 -0300

    feat: config, logging and tracing

--- infra/src/env/configs.rs
+++ infra/src/env/configs.rs
@@ -11,14 +11,15 @@ pub enum Environment {
 pub struct Config {
     pub app_name: &'static str,
     pub env: Environment,
+    pub app_host: &'static str,
+    pub app_port: u64,
+    pub log_level: &'static str,
+    pub enable_rumqttc_logging: bool,
 
     pub mqtt_host: &'static str,
     pub mqtt_port: u16,
     pub mqtt_user: &'static str,
     pub mqtt_password: &'static str,
-
-    pub log_level: &'static str,
-    pub enable_rumqttc_logging: bool,
 
     pub amqp_host: &'static str,
     pub amqp_port: u16,
@@ -42,6 +43,8 @@ impl Config {
     pub fn new() -> Box<Self> {
         Box::new(Config {
             app_name: "rust_iot",
+            app_host: "local",
+            app_port: 12345,
             env: Environment::Local,
             log_level: "debug",
             enable_rumqttc_logging: false,
@@ -70,6 +73,10 @@ impl Config {
         })
     }
 
+    pub fn app_addr(&self) -> String {
+        format!("{}:{}", self.app_host, self.app_port)
+    }
+
     pub fn amqp_uri(&self) -> String {
         format!(
             "amqp://{}:{}@{}:{}{}",
@@ -88,6 +95,8 @@ impl Config {
     pub fn mock() -> Box<Self> {
         Box::new(Config {
             app_name: "rust_iot",
+            app_host: "local",
+            app_port: 12345,
             env: Environment::Local,
             mqtt_host: "localhost",
             mqtt_port: 1883,
--- s.grpc/src/main.rs
+++ s.grpc/src/main.rs
@@ -1,9 +1,8 @@
 mod services;
 
-use std::sync::Arc;
-
 use app::ExampleServiceImpl;
-use infra::{env::Config, repositories::iot_repository::IoTRepositoryImpl};
+use infra::{env::Config, logging, otel, repositories::iot_repository::IoTRepositoryImpl};
+use log::debug;
 use protos::iot::iot_data_server::IotDataServer;
 use services::iot;
 use sqlx::postgres::PgPoolOptions;
@@ -11,8 +10,13 @@ use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let cfg = Config::new();
+    let mut cfg = Config::new();
+    cfg.app_name = "ggrpc";
+    cfg.app_host = "[::1]";
+    cfg.app_port = 50051;
+
+    logging::setup(&cfg)?;
+    otel::tracing::setup(&cfg)?;
 
     let pool = Box::new(
         PgPoolOptions::new()
@@ -25,10 +29,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let service = ExampleServiceImpl::new(repository);
     let iot_service = iot::IoTGrpcService::new(service);
 
-    println!("starting server...");
+    debug!("starting server...");
     Server::builder()
         .add_service(IotDataServer::new(iot_service))
-        .serve(addr)
+        .serve(cfg.app_addr().parse()?)
         .await?;
 
     Ok(())
